1792f20f9ec86729d033aa0c99c2b992f5123912
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2018-06-11 17:01:56 -0600

    Add default configuration file.

---
+++ config.sh
@@ -0,0 +1,25 @@
+
+
+BOOT_TYPE=UEFI # UEFI or BIOS
+DISK=sdc
+EFI_PART=1  # ignored if BOOT_TYPE is BIOS
+ROOT_PART=1
+RPOOL=rpool  # name of the root pool
+# Specify which paths should be thier own ZFS filesystems.
+# Comment out those you do not want.
+SWAP=auto
+FILESYSTEMS=(
+    local           # /usr/local
+    opt             # /opt
+    srv             # /srv - noexec
+    games           # /var/games
+    mysql           # /var/lib/mysql
+    postgres        # /var/lib/postgres
+    mongodb         # /var/lib/mongodb
+    libvirt         # /var/lib/libvirt
+    nfs \           # /var/lib/nfs - no snapshots
+    mail            # /var/mail
+    user-cache      # /home/USER/.cache - no snapshots
+    user-downloads  # /home/USER/Downloads - no snapshots
+    user-scratch    # /home/USER/Scratch - no snapshosts
+    )
